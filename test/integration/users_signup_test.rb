7011f549e0943902dae6e07edf943a5ea6cf0484
Toba <marxdm02@gmail.com>
2020-07-19 14:03:03 +0000

    Session

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,9 +6,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
     end
     assert_template 'users/new'
   end
